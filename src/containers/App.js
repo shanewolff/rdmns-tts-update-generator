3dec59c395da9c607db5b63dab8ac1a2c67b4587
Shane Wolff <wolffshane@gmail.com>
2020-08-08 23:57:27 +0530

    Restructured the project to remove default react stylings

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<React.Fragment>
+			<h1>RDMNS TTS Update Generator</h1>
+			<p>This is a small description</p>
+		</React.Fragment>
+
+	);
+
+}
+
+export default App;
